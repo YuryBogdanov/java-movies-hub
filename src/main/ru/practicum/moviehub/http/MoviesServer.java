a6ec76dec0bbdd4570b366562878cc7b38929782
Yury Bogdanov <yury-bogdanov@mail.ru>
2026-08-08 23:13:06 +0300

    implemented server starting

--- src/main/ru/practicum/moviehub/http/MoviesServer.java
+++ src/main/ru/practicum/moviehub/http/MoviesServer.java
@@ -1,5 +1,31 @@
 package ru.practicum.moviehub.http;
 
-public class MoviesServer {
+import com.sun.net.httpserver.HttpServer;
+import ru.practicum.moviehub.store.MoviesStore;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class MoviesServer {
+    private HttpServer server;
+    private MoviesStore store;
+
+    public MoviesServer(MoviesStore store, int port) {
+        this.store = store;
+
+        try {
+            server = HttpServer.create(new InetSocketAddress(port), 0);
+        } catch (IOException e) {
+            // TODO: Make proper error handling
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public void start() {
+        server.start();
+    }
+
+    public void stop() {
+        server.stop(0);
+    }
 }
