fd0656295cef55799c4fe8a048d49a8ce7535853
Yury Bogdanov <yury-bogdanov@mail.ru>
2026-08-16 16:26:41 +0300

    prevent duplication

--- src/main/ru/practicum/moviehub/store/MoviesStore.java
+++ src/main/ru/practicum/moviehub/store/MoviesStore.java
@@ -3,15 +3,30 @@ package ru.practicum.moviehub.store;
 import ru.practicum.moviehub.model.Movie;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
 
 public class MoviesStore {
     private HashMap<String, Movie> storage = new HashMap<>();
 
     public String storeMovie(Movie movie) {
-        String id = java.util.UUID.randomUUID().toString();
+        Optional<String> possibleId = checkIfMovieExists(movie);
+        if (possibleId.isPresent()) {
+            return possibleId.get();
+        } else {
+            String id = java.util.UUID.randomUUID().toString();
+            storage.put(id, movie);
+            return id;
+        }
+    }
 
-        storage.put(id, movie);
-
-        return id;
+    private Optional<String> checkIfMovieExists(Movie movie) {
+        return storage
+                .entrySet()
+                .stream()
+                .filter(m -> m.getValue().getTitle().equals(movie.getTitle())
+                && m.getValue().getYear() == movie.getYear())
+                .map(Map.Entry::getKey)
+                .findFirst();
     }
 }
