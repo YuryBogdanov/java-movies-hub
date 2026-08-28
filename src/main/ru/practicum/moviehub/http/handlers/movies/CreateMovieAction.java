ef86f09bb9303c6c5cac54c97932b8972a14e565
Yury Bogdanov <yury-bogdanov@mail.ru>
2026-08-28 22:09:59 +0300

    self review fixes

--- src/main/ru/practicum/moviehub/http/handlers/movies/CreateMovieAction.java
+++ src/main/ru/practicum/moviehub/http/handlers/movies/CreateMovieAction.java
@@ -87,27 +87,3 @@ public class CreateMovieAction implements MovieAction {
         return minYear <= year && year <= java.time.LocalDate.now().getYear() + 1;
     }
 }
-
-class CreateMovieRequestValidationResult {
-    private final String titleValidationResult;
-    private final String yearValidationResult;
-
-    CreateMovieRequestValidationResult(String titleValidationResult, String yearValidationResult) {
-        this.titleValidationResult = titleValidationResult;
-        this.yearValidationResult = yearValidationResult;
-    }
-
-    String getTitleValidationResult() {
-        return titleValidationResult;
-    }
-
-    String getYearValidationResult() {
-        return yearValidationResult;
-    }
-
-    List<String> getValidationMessages() {
-        return Stream.of(getTitleValidationResult(), getYearValidationResult())
-                .filter(Objects::nonNull)
-                .collect(Collectors.toList());
-    }
-}
